a2441149c308a1e5d9d20f5e7d9406d5fddd9704
kazu0914 <kazunori-t@cyberbra.in>
2023-04-12 12:34:47 +0900

    Add files via upload
    first commits

---
+++ chatgpt_go_on/content.js
@@ -0,0 +1,31 @@
+// CSSファイルを読み込む
+const style = document.createElement('link');
+style.rel = 'stylesheet';
+style.type = 'text/css';
+style.href = chrome.runtime.getURL('styles/style.css');
+document.head.appendChild(style);
+
+// サンプルボタンを作成
+const sampleButton = document.createElement('button');
+sampleButton.innerText = 'Go on';
+sampleButton.classList.add('go-on-button');
+
+// サンプルボタンをページに追加
+document.body.appendChild(sampleButton);
+
+sampleButton.addEventListener('click', () => {
+    const xpath = '//*[@id="__next"]/div[2]/div[2]/main/div[2]/form/div/div[2]/textarea';
+    const iterator = document.evaluate(xpath, document, null, XPathResult.ANY_TYPE, null);
+    const textarea = iterator.iterateNext();
+  
+    if (textarea) {
+      textarea.value = 'go on';
+    } else {
+      console.log('Textarea not found...');
+    }
+});
+// ボタンをチャット入力欄の右側に表示
+const chatInputArea = document.querySelector('textarea');
+const chatInputWrapper = chatInputArea.parentElement;
+chatInputWrapper.style.position = 'relative';
+chatInputWrapper.appendChild(sampleButton);
