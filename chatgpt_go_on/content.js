1dce62b8b43f7e0ad7b5e68f274075cde69477ec
kazu0914 <kazunori-t@cyberbra.in>
2023-04-12 17:19:06 +0900

    edit
    既に会話が行われているスレッドページでも「Go on」ボタンが読み込まれるように修正

--- chatgpt_go_on/content.js
+++ chatgpt_go_on/content.js
@@ -1,25 +1,41 @@
-
 // サンプルボタンを作成
 const sampleButton = document.createElement('button');
 sampleButton.innerText = 'Go on';
 sampleButton.classList.add('go-on-button');
 
-// サンプルボタンをページに追加
+// サンプルボタンをChatGPTの画面に追加し表示
 document.body.appendChild(sampleButton);
 
 sampleButton.addEventListener('click', () => {
-    const xpath = '//*[@id="__next"]/div[2]/div[2]/main/div[2]/form/div/div[2]/textarea';
-    const iterator = document.evaluate(xpath, document, null, XPathResult.ANY_TYPE, null);
-    const textarea = iterator.iterateNext();
-  
-    if (textarea) {
-      textarea.value = 'go on';
-    } else {
-      console.log('Textarea not found...');
-    }
+  const xpath = '//*[@id="__next"]/div[2]/div[2]/main/div[2]/form/div/div[2]/textarea';
+  const iterator = document.evaluate(xpath, document, null, XPathResult.ANY_TYPE, null);
+  const textarea = iterator.iterateNext();
+
+  if (textarea) {
+    textarea.value = 'go on';
+  } else {
+    console.log('Textarea not found...');
+  }
 });
-// ボタンをチャット入力欄の右上に表示
-const chatInputArea = document.querySelector('textarea');
-const chatInputWrapper = chatInputArea.parentElement;
-chatInputWrapper.style.position = 'relative';
-chatInputWrapper.appendChild(sampleButton);
+
+// ボタンをチャット入力欄の右上に表示する関数
+function addButton() {
+  const chatInputArea = document.querySelector('textarea');
+  if (chatInputArea) {
+    const chatInputWrapper = chatInputArea.parentElement;
+    chatInputWrapper.style.position = 'relative';
+    chatInputWrapper.appendChild(sampleButton);
+  }
+}
+
+// ページが読み込まれた時にボタンを追加
+addButton();
+
+// ページの変更を監視して新しいチャットエリアが表示された場合にボタンを追加
+const observer = new MutationObserver(() => {
+  if (!document.contains(sampleButton)) {
+    addButton();
+  }
+});
+
+observer.observe(document.body, { childList: true, subtree: true });
